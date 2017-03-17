de54aa9b480dfec0fad45396ccb3b077ba51517e
Douglas <douglas@chippy.ch>
2017-03-17 21:09:30 +0000

    Fix version

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_ips():
 
 
 git_version = subprocess.check_output(
-    ['git', 'describe', '--all', '--long'])[:-1].decode('utf-8')
+    ['git', 'describe', '--long'])[:-1].decode('utf-8')
 
 
 def md5_to_file(md5):
