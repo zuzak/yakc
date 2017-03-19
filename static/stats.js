55375bc1b5bd28970599e58a51d774cb979feb50
Douglas Gardner <douglas@chippy.ch>
2017-03-19 05:06:51 +0000

    Tidy code

--- static/stats.js
+++ static/stats.js
@@ -2,11 +2,13 @@
 var createDivs = function ( data ) {
 	var body = document.querySelector( '.counter');
 	var queues = [ 'best', 'held', 'good', 'pending', 'bad', 'trash' ];
-	var div = document.createElement( 'div' );
-	div.className = 'delta';
-	div.id = 'delta';
-	div.innerHTML = 'Δ0';
-	body.appendChild(div);
+
+	var delta = document.createElement( 'div' );
+	delta.className = 'delta';
+	delta.id = 'delta';
+	delta.innerHTML = 'Δ0';
+	body.appendChild(delta);
+
 	for ( var i = 0; i < queues.length; i++  ) {
 		var queue = queues[i];
 		var div = document.createElement( 'div' );
@@ -28,26 +30,11 @@ var createDivs = function ( data ) {
 		body.appendChild( span );
 	}
 };
-var getJSON = function(url, callback) {
-	// http://stackoverflow.com/a/35970894
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status);
-      }
-    };
-    xhr.send();
-};
-
 var updateCounts = function ( data ) {
+	var div;
 	for ( var queue in data.counts ) {
 		if ( data.counts.hasOwnProperty( queue ) ) {
-			var div = document.getElementById( queue );
+			div = document.getElementById( queue );
 			if ( !div ) { continue; }
 			div.innerHTML = data.counts[queue] + ' ' + queue;
 			var size = 0.5 * Math.sqrt(data.counts[queue]) + 'em';
@@ -68,7 +55,24 @@ var updateCounts = function ( data ) {
 		div.className = 'delta';
 	}
 	setTimeout(poll, 3000);
-}
+};
+
+var getJSON = function(url, callback) {
+	// http://stackoverflow.com/a/35970894
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+
 
 var poll = function () {
 	getJSON('//api.webm.website/stats.json', function ( e, r ) {
