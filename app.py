zuzak/yakc

commit a810625387e8ee540c69958aa926ad2a04cc7e68

More code style tweaks

app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS, cross_origin
 from random import choice, random
 from uuid import uuid4
 import json
-import logging
 from hashlib import sha256
 import shutil
 import socket
@@ -56,7 +55,7 @@ def md5_to_file(md5):
 
 
 def get_ip():
-    if (request.environ.get('HTTP_X_REAL_IP')):
+    if request.environ.get('HTTP_X_REAL_IP'):
         return request.environ.get('HTTP_X_REAL_IP')
     elif request.environ.get('X-Forwarded-For'):
         return request.environ.get('X-Forwarded-For')
@@ -65,7 +64,6 @@ def get_ip():
 
 
 def add_log(webm, action):
-    global delta
     ip = get_user()
     string = strftime('%Y-%m-%d %H:%M:%S ' + ip + ' ' + action)
     if ip != get_ip() and action != 'viewed':
@@ -118,10 +116,7 @@ def user_banned():
         with open('bans.txt') as text:
             bans = text.read().splitlines()
 
-            if get_ip() in bans:
-                return True
-            else:
-                return False
+            return bool(get_ip() in bans)
     except FileNotFoundError:
         return False
 
@@ -295,7 +290,7 @@ def privacy():
 
 @app.route('/<name>.webm')
 @app.route('/<name>.webm', subdomain='<domain>')
-def serve_webm(name, domain=None):
+def serve_webm(name):
     if request.accept_mimetypes.best_match(
             ['video/webm', 'text/html']) == 'text/html':
         return redirect(name)
@@ -321,7 +316,7 @@ def metadata_exists(webm):
 
 @app.route('/<name>', subdomain='<domain>')
 @app.route('/<name>')
-def show_webm(name, domain=None):
+def show_webm(name):
     name = name + '.webm'
     queue = 'pending'
     token = None
@@ -641,10 +636,10 @@ def change_nick():
 
 @app.route('/moderate', methods=['POST'])
 @app.route('/moderate', methods=['POST'], subdomain='<domain>')
-def moderate_webm(domain=None):
+def moderate_webm():
     webm = request.form['webm']
     token = request.form['token'].split(':')
-    if not (token[0] + ':' + token[1] == generate_webm_token(webm, token[1])):
+    if not token[0] + ':' + token[1] == generate_webm_token(webm, token[1]):
         abort(400, 'token mismatch')
 
     verdict = request.form['verdict']

static/stats.js
@@ -10,7 +10,7 @@ var createDivs = function ( data ) {
 	for ( var i = 0; i < queues.length; i++  ) {
 		var queue = queues[i];
 		var div = document.createElement( 'div' );
-		if ( queue == 'total' ) {
+		if ( queue === 'total' ) {
 			continue;
 		}
 
@@ -27,7 +27,7 @@ var createDivs = function ( data ) {
 		span.innerHTML = '&#x2195;';
 		body.appendChild( span );
 	}
-}
+};
 var getJSON = function(url, callback) {
 	// http://stackoverflow.com/a/35970894
     var xhr = new XMLHttpRequest();
@@ -35,7 +35,7 @@ var getJSON = function(url, callback) {
     xhr.responseType = 'json';
     xhr.onload = function() {
       var status = xhr.status;
-      if (status == 200) {
+      if (status === 200) {
         callback(null, xhr.response);
       } else {
         callback(status);
@@ -46,13 +46,15 @@ var getJSON = function(url, callback) {
 
 var updateCounts = function ( data ) {
 	for ( var queue in data.counts ) {
-		var div = document.getElementById( queue );
-		if ( !div ) { continue; }
-		div.innerHTML = data.counts[queue] + ' ' + queue;
-		var size = 0.5 * Math.sqrt(data.counts[queue]) + 'em';
-		div.style.height = size;
-		div.style.width = size;
-		div.style['line-height'] = size;
+		if ( data.counts.hasOwnProperty( queue ) ) {
+			var div = document.getElementById( queue );
+			if ( !div ) { continue; }
+			div.innerHTML = data.counts[queue] + ' ' + queue;
+			var size = 0.5 * Math.sqrt(data.counts[queue]) + 'em';
+			div.style.height = size;
+			div.style.width = size;
+			div.style['line-height'] = size;
+		}
 	}
 
 

webms/dedupe.py
@@ -2,7 +2,6 @@
 import os
 import hashlib
 from time import strftime
-import sys
 
 hashes = dict()
 FN = 'all'

commit da25b64142b00508f03395a0e7146d350ec365f6

Re-add domain arguments
Reverts part of a810625387e8ee540c69958aa926ad2a04cc7e68, which
broke everything

app.py
@@ -290,7 +290,7 @@ def privacy():
 
 @app.route('/<name>.webm')
 @app.route('/<name>.webm', subdomain='<domain>')
-def serve_webm(name):
+def serve_webm(name, domain=None):
     if request.accept_mimetypes.best_match(
             ['video/webm', 'text/html']) == 'text/html':
         return redirect(name)
@@ -316,7 +316,7 @@ def metadata_exists(webm):
 
 @app.route('/<name>', subdomain='<domain>')
 @app.route('/<name>')
-def show_webm(name):
+def show_webm(name, domain=None):
     name = name + '.webm'
     queue = 'pending'
     token = None
@@ -635,7 +635,7 @@ def change_nick():
 
 @app.route('/moderate', methods=['POST'])
 @app.route('/moderate', methods=['POST'], subdomain='<domain>')
-def moderate_webm():
+def moderate_webm(domain=None):
     webm = request.form['webm']
     token = request.form['token'].split(':')
     if not token[0] + ':' + token[1] == generate_webm_token(webm, token[1]):